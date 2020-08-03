4348d516289132e0bb3e1ecbcecdc6ddc038fb76
Ole André Vadla Ravnås <oleavr@gmail.com>
2020-08-04 01:06:15 +0200

    Modernize the plugin header

--- src/plugin.h
+++ src/plugin.h
@@ -6,11 +6,11 @@
 class FridaQmlPlugin : public QQmlExtensionPlugin
 {
     Q_OBJECT
-    Q_PLUGIN_METADATA(IID "org.qt-project.Qt.QQmlExtensionInterface")
+    Q_PLUGIN_METADATA(IID QQmlExtensionInterface_iid)
 
 public:
-    void registerTypes(const char *uri);
-    void initializeEngine(QQmlEngine *engine, const char *uri);
+    void registerTypes(const char *uri) override;
+    void initializeEngine(QQmlEngine *engine, const char *uri) override;
 };
 
 #endif
